c750c25f365d8bd88fe32d4b59761bf287a60593
santhoshpackirisamy <santhoshpackirisamy007@gmail.com>
2020-05-05 19:12:30 +0530

    aesfds;
    dsf

--- app/routes/child.js
+++ app/routes/child.js
@@ -2,14 +2,5 @@ import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 
 export default Route.extend({
-    // queryParams: {
-    //     property:  {refreshModel: true}
-    //   },
-    model(params)
-    {
-        if(params.property)
-        {
-            console.log(params.property);
-        }
-    }
+
 });
