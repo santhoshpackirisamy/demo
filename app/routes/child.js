e2669b3505448d15659e4cc8bbe5e43d20740115
santhoshpackirisamy <santhoshpackirisamy007@gmail.com>
2020-05-05 19:26:20 +0530

    Revert "aesfds;"
    This reverts commit c750c25f365d8bd88fe32d4b59761bf287a60593.

--- app/routes/child.js
+++ app/routes/child.js
@@ -2,5 +2,14 @@ import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 
 export default Route.extend({
-
+    // queryParams: {
+    //     property:  {refreshModel: true}
+    //   },
+    model(params)
+    {
+        if(params.property)
+        {
+            console.log(params.property);
+        }
+    }
 });
